c9120d71106130c22d79b6b7d5380a2c70707169
Timothy Gunkel <gunk1221@Timothys-MacBook-Air.local>
2018-10-19 11:34:17 -0500

    Column Adjustment

--- Fish-analysis.R
+++ Fish-analysis.R
@@ -3,5 +3,5 @@ library(tidyverse)
 .libPaths("/Users/gunk1221/Documents/RLibrary")
 source("/Users/gunk1221/Desktop/Graduate School Course Folders /NRES 800 - 001 Sampling and Data MGT & Visual/Lecture/gittutorial/R/Themes.R")
 Fish %>% 
-  mutate(length_cat=case_when(length>=200~"Big",length<200~"Small"))->Fish
+  mutate(length_cat=case_when(length>=300~"Big",length<300~"Small"))->Fish
 head(Fish)
